3b042bf7eb4153ac27a3218e1d0567cf45b05dbf
flepponier <106471189+flepponier@users.noreply.github.com>
2023-02-10 22:10:14 +0100

    New Stuff

---
+++ app/src/main/java/de/app/rentalkiezapp/activity/RentObjectActivity.java
@@ -0,0 +1,14 @@
+package de.app.rentalkiezapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RentObjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rent_object_activity);
+    }
+}
---
+++ app/src/main/java/de/app/rentalkiezapp/activity/RentResultsActivity.java
@@ -0,0 +1,14 @@
+package de.app.rentalkiezapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RentResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rent_results_activity);
+    }
+}
